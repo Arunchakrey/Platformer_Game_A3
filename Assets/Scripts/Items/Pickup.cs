33f9b91e43db1a294946c378f428a1cdcef1a389
Arunchakrey Puthyrith <104348076@student.swin.edu.au>
2025-10-22 14:38:02 +1100

    Slider bar

--- Assets/Scripts/Items/Pickup.cs
+++ Assets/Scripts/Items/Pickup.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class Pickup : MonoBehaviour, IItem
 {
     public static event Action<int> OnSapplingCollect;
-    public int worth = 5;
+    public int worth = 10;
     public void Collect()
     {
-        OnSapplingCollect.Invoke(worth);
         Destroy(gameObject);
+        OnSapplingCollect.Invoke(worth);
     }
 }
 
