2237023a4079ecdbfc01cc54170b64a9835c21d3
Arunchakrey <104348076@student.swin.edu.au>
2025-10-29 13:22:09 +1100

    fixes to slider

--- Assets/Scripts/UI/Score/GameController.cs
+++ Assets/Scripts/UI/Score/GameController.cs
@@ -1,28 +1,65 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    private int progressAmount;
-    public Slider progressSlider;
-    private void Start()
-    {
-        progressAmount = 0;
-        progressSlider.value = 0;
-        Pickup.OnSapplingCollect += IncreaseProgressAmount;
-    }
+    public static GameController Instance { get; private set; }
 
-    private void IncreaseProgressAmount(int amount)
-    {
-        progressAmount += amount;
-        progressSlider.value = progressAmount;
+    [SerializeField] private Slider progressSlider;
+    [SerializeField] private Transform saplingsParent; // optional
 
-        if (progressAmount >= 100)
+    private int totalSaplings;
+    private int collected;
+    private bool subscribed;
+
+    private void Awake()
+    {
+        // Singleton guard (prevents double subscriptions)
+        if (Instance != null && Instance != this)
         {
-            //level complete
-            Debug.Log("Level Complete");
+            Destroy(gameObject);
+            return;
         }
+        Instance = this;
+
+        // Count by component (includes nested children). If no parent, scan whole scene.
+        totalSaplings = saplingsParent
+            ? saplingsParent.GetComponentsInChildren<Pickup>(true).Length
+            : FindObjectsOfType<Pickup>(true).Length;
+
+        if (totalSaplings <= 0) totalSaplings = 1;
+
+        // Slider config
+        progressSlider.wholeNumbers = true;
+        progressSlider.minValue = 0;
+        progressSlider.maxValue = totalSaplings;
+        progressSlider.value = 0;
+        collected = 0;
+
+        Debug.Log($"[GC] Saplings found = {totalSaplings}");
     }
 
+    private void OnEnable()
+    {
+        if (subscribed) return;
+        Pickup.OnSapplingCollect += HandleSaplingPickup;
+        subscribed = true;
+    }
+
+    private void OnDisable()
+    {
+        if (!subscribed) return;
+        Pickup.OnSapplingCollect -= HandleSaplingPickup;
+        subscribed = false;
+    }
+
+    private void HandleSaplingPickup(int _amount)
+    {
+        // Always count one sapling per collect
+        collected = Mathf.Min(collected + 1, totalSaplings);
+        progressSlider.value = collected;
+
+        if (collected >= totalSaplings)
+            Debug.Log("[GC] Level complete!");
+    }
 }
